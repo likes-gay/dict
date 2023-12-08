fb4b98f733e0d0fedcfbe776777a27baf2ee6ec0
Yummy_Bacon5 <68166338+YummyBacon5@users.noreply.github.com>
2023-12-08 19:59:53 +0000

    Upgrate to TinyDB!!
    Co-authored-by: Luke Thomas
    <Zoobdude@users.noreply.github.com>

--- README.md
+++ README.md
@@ -4,21 +4,22 @@
 
 - [x] Add updoot button
 - [x] Random word API endpoint
-- [ ] Migrate to a database rather than json file. Potentially [pickle db](https://pythonhosted.org/pickleDB/)
-- [ ] Implment char limits for each json value
+- [x] Migrate to a database rather than JSON file. Potentially [tiny db](https://tinydb.readthedocs.io/en/latest/)
+- [ ] Implment char limits for each JSON value
 - [ ] Improve security, require API key?
 - [ ] Add a way to delete entries
 - [ ] Add a way to edit entries
 - [ ] Add a way to search entries
-- [ ] Add a way to sort entries
-- [ ] Add a way to filter entries
+- [x] Add a way to sort entries
 - [ ] Add a way to export entries
 - [ ] Add a way to import entries
 - [ ] Add a way to view entries by date
 - [ ] Filter by updoot count
+- [ ] Add a way to filter entries
+- [x] Order by updoot count
 - [ ] Downdoot button?
 - [ ] Usage counter
-- [ ] Add a way to view entries by user
+- [x] Add a way to view entries by user
 - [ ] Add word catergories
 - [ ] Add a way to view entries by word catergory
 - [ ] Add validation on acepted query params more effeciently
--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,27 @@
+from enum import Enum
 import time
-from typing	import List, Union
-from typing_extensions import Annotated
-from fastapi import	Cookie, FastAPI, HTTPException,	Query, Request, Response
+
+from fastapi import	FastAPI, HTTPException,	Query
 from fastapi.middleware.cors import	CORSMiddleware
-from numpy import array
 from pydantic import BaseModel
-import json
+
 from random_word import	RandomWords
 from random	import choice
 
+from tinydb	import TinyDB, Query
+from tinydb.operations import increment
+
+from os import getenv
+
+db = TinyDB("db_data.json")
 app	= FastAPI()
 
 app.add_middleware(
 	CORSMiddleware,
 	allow_origins=["*"],
-	allow_methods=["GET", "POST"],
+	allow_methods=["GET", "POST", "DELETE"],
 	allow_headers=["Content-Type"],
 )
-
-# ------------------------------------------
-
-def	read_json():
-	with open("data.json", "r")	as f:
-		return json.loads(f.read())
 
 # -------------------------------------------
 
@@ -36,10 +35,18 @@ class UploadWordFormat(BaseModel):
 	creationDate: int
 	uploader: str
 
+class DeleteWord(BaseModel):
+	id: int
+	secretKey: str
+
+class DirEnum(str, Enum):
+    desc = "desc"
+    asc = "asc"
+
 # -------------------------------------------
 
 #@app.middleware("http")
-#async def session_cookie(req: Request, cookie: Cookie(default=None)):
+#async def session_cookie(req: Request,	cookie:	Cookie(default=None)):
 #	print(cookie)
 
 # -------------------------------------------
@@ -51,103 +58,133 @@ async def root():
 # -------------------------------------------
 
 @app.get("/num_of_words")
-async def num_of_words():
-	data = read_json()
-	count_of_words = len(data)
-	return {"totalWords": count_of_words}
-
-@app.get("/get_word/{id}")
-async def get_word(id: int):
-	data = read_json()
-	for	word in data:
-		if word["id"] == id:
-			return word
-	raise HTTPException(status_code=404, detail="Item not found	lol")
+async def count_of_words():
+	return {"totalWords": len(db)}
 
 @app.post("/upload_word", status_code=200)
-async def upload_word(newWord: UploadWordFormat):
-	with open("data.json", "r+") as f:
-		file_data =	read_json()
-		newWordDict = newWord.model_dump()
+async def upload_new_word(newWord: UploadWordFormat):
+	#check if word is empty
+	if newWord.word	== "":
+		raise HTTPException(status_code=400, detail="Word cannot be empty")
+
+	#check if word already exists
+	if newWord.word	in [entry["word"] for entry	in db.all()]:
+		raise HTTPException(status_code=400, detail="Word already exists")
+	
+	if newWord.description == "":
+		raise HTTPException(status_code=400, detail="Description cannot	be empty")
+
+	if not newWord.uploader:
+		uploader = "Anonymous"
+	else:
+		uploader = newWord.uploader
+	
+	if not newWord.creationDate:
+		newWord.creationDate = int(time.time())
+	
+	record = {
+			"id": len(db),
+			"word":	newWord.word,
+			"description": newWord.description,
+			"creationDate":	newWord.creationDate,
+			"uploader":	uploader.lower(),
+			"updoots": 0
+		}
+	
+	db.insert(record)
+
+	return(record)
+
+@app.delete("/delete_word/{id}")
+async def delete(req: DeleteWord):
+	if req.secretKey != getenv("SECRET_KEY"):
+		raise HTTPException(403, detail="Unauthorised, invalide secret key")
+	
+	
 		
-		newWordDict["id"] = file_data[-1]["id"] + 1
-		file_data.append(newWordDict)
-		
-		if not newWordDict["creationDate"]:
-			newWordDict["creationDate"] = int(time.time())
-		
-		f.seek(0)
-		f.write(json.dumps(file_data))
-		f.truncate()
-		return newWordDict
+
 
 @app.post("/update_updoot")
-async def update_updoot(req: UpdateUpdoot):
-	data = read_json()
-	for	word in data:
-		if word["id"] == req.id:
-			if req.isUpdooted:
-				word["updoots"]	+= 1
-			else:
-				word["updoots"]	-= 1
-			
-			with open("data.json", "w")	as f:
-				f.write(json.dumps(data))
-			break
+async def update_words_updoot_count(req: UpdateUpdoot):
+	if req.isUpdooted == None:
+		raise HTTPException(status_code=400, detail="isUpdooted	cannot be None")
 
-@app.get("/get_random_word")
-async def get_random_dctionary_word():
-	return {
-		"word":	choice(read_json()),
-		"realRandomWord": RandomWords().get_random_word()
-	}
+	if req.id == None:
+		raise HTTPException(status_code=400, detail="id	cannot be None")
+	
+	if req.isUpdooted == True:
+		db.update(increment("updoots"),	Query().id == req.id)
+	
+	response = db.search(Query().id	== req.id)
+	return(response[0])
+		
+@app.get("/get_word/{id}")
+async def get_word_by_ID(id: int):
+	response = db.search(Query().id	== id)
+	
+	if response:
+		return response[0]
+	else:
+		raise HTTPException(status_code=404, detail="Item not found	lol")
 
 @app.get("/get_all_words")
-async def get_all_words(order_by: str = "id", dir: str = "as"): #Query param validation not working here #Annotated[Union[str, None], Query()] = ["id", "updoot"]
-	data = read_json()
-	
+async def get_all_words(order_by: str = "id", dir: DirEnum = "desc"):
 	valid_order_by = "id"
 
-	if order_by in ["id", "updoot", "updoots"]:
+	if order_by	in ["id", "updoot",	"updoots"]:
 		valid_order_by = order_by
-		if order_by == "updoot":
+		if order_by	== "updoot":
 			valid_order_by = "updoots"
 	else:
 		raise HTTPException(status_code=400, detail="Invalid query params")
 	
-	return sorted(data,	key=lambda x: x[valid_order_by])
-	
+	if dir == "asc":
+		return sorted(db.all(),	key=lambda x: x[valid_order_by])
 
-@app.get("/all_words_unsorted")
-def all_words():
-	data = read_json()
-	return(data)
+	elif dir == "desc":
+		return sorted(db.all(),	key=lambda x: x[valid_order_by], reverse=True)
+	
+	return sorted(db.all(),	key=lambda x: x[valid_order_by])
+
 
 @app.get("/get_range_of_words")
-async def get_range_of_words(offset: int = 0, size: int = 5):
-	data = read_json()
+async def get_range_of_words(offset: int = 0, size:	int	= 5):
+	data = db.all()
 
-	if size < 1:
+	if size	< 1:
 		raise HTTPException(status_code=400, detail="Size cannot be less than 1")
-					  
-	if offset < 0:
-		raise HTTPException(status_code=400, detail="Offset cannot be less than 0")
+						
+	if offset <	0:
+		raise HTTPException(status_code=400, detail="Offset	cannot be less than	0")
 
-	if offset > len(data):
-		raise HTTPException(status_code=400, detail="Offset cannot be greater than the length the data")
+	if offset >	len(data):
+		raise HTTPException(status_code=400, detail="Offset	cannot be greater than the length the data")
 
 	return {
-		"dictWords": data[offset : offset + size],
+		"dictWords": data[offset : offset +	size],
 		"max": len(all_words())
 	}
- 
-@app.get("/lookup_id_of_word/{word}")
-async def lookup_id(word: str):
-	data = read_json()
 	
-	for line in data:
-		if line["word"].lower() == word.lower():
-			return {
-				"id": line["id"]
-			}
-	raise HTTPException(status_code=404, detail="Item not found lol")
+@app.get("/lookup_id/{word}")
+async def lookup_id_of_word(word: str):
+	response = db.search(Query().word == word)
+	if response:
+		return {"id": response[0]["id"]}
+
+	else:
+		raise HTTPException(status_code=404, detail="Item not found	lol")
+
+
+@app.get("/get_uploaders_posts/{uploader}")
+async def get_all_of_a_uploaders_posts(uploader: str):
+	response = db.search(Query().uploader == uploader.lower())
+
+	return(response)
+
+
+@app.get("/get_random_word")
+async def get_a_random_word_and_one_from_the_english_dictionary():
+	return {
+		"word":	choice(db.all())["word"],
+		"realRandomWord": RandomWords().get_random_word()
+	}
