5821dd03d00adcaed910a66de2dc56a11bebd4da
Yummy_Bacon5 <68166338+YummyBacon5@users.noreply.github.com>
2024-01-29 13:35:43 +0000

    Change indentation to tabs, and remove an unneeded space

--- frontend/src/hooks/utils.tsx
+++ frontend/src/hooks/utils.tsx
@@ -22,7 +22,7 @@ export function LinkIcon() {
 
 export function UpdootIcon() {
 	return (
-		<svg  className="arrow-icon" width={30} height={30} viewBox="0 0 256 256">
+		<svg className="arrow-icon" width={30} height={30} viewBox="0 0 256 256">
 			<path
 				d="M231.39062,123.06152A8,8,0,0,1,224,128H184v80a16.01833,16.01833,0,0,1-16,16H88a16.01833,16.01833,0,0,1-16-16V128H32a8.00065,8.00065,0,0,1-5.65723-13.65723l96-96a8.003,8.003,0,0,1,11.31446,0l96,96A8.002,8.002,0,0,1,231.39062,123.06152Z"
 			/>
